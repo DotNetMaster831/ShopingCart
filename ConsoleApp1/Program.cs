2b51dc7d81622ccc48a9848fe5e953c45430e443
Durga Sai Kandregula <durgasai831@gmail.com>
2024-12-18 12:16:54 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Mime;
+using System.Windows;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [STAThread] // Required for WPF
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Launching WPF Window from Console Application...");
+
+            // Create a WPF Application instance
+            var app = new Application();
+
+            // Instantiate the WPF Window from MyLibrary
+            var window = new WpfLibrary1.Window1();
+
+            // Run the WPF Application with the Custom Window
+            app.Run(window);
+
+            Console.WriteLine("WPF Window closed. Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        [STAThread] // Required for WPF
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Launching WPF Window from Console Application...");
+
+                // Create a WPF Application instance
+                var app = new Application();
+
+                // Instantiate the WPF Window from MyLibrary
+                var window = new WpfLibrary2.Window2();
+
+                // Run the WPF Application with the Custom Window
+                app.Run(window);
+
+                Console.WriteLine("WPF Window closed. Press any key to exit.");
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
